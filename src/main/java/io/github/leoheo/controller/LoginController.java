72f08b014c8660a3ccba373bdab10b2165ee0687
JinHan <hjh5488@gmail.com>
2018-03-19 20:52:18 +0900

    Refactor package

--- src/main/java/io/github/leoheo/controller/LoginController.java
+++ src/main/java/io/github/leoheo/controller/LoginController.java
@@ -1,4 +1,4 @@
-package io.github.leoheo.post;
+package io.github.leoheo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/io/github/leoheo/controller/PostController.java
+++ src/main/java/io/github/leoheo/controller/PostController.java
@@ -1,7 +1,6 @@
-package io.github.leoheo.post;
+package io.github.leoheo.controller;
 
 import java.security.Principal;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
